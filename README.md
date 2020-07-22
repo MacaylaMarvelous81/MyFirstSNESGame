a2209311071c14ef169e3d73a0b3036bf5fd8834
MacaylaMarvelous81 <admin@thelegendaryorb.tk>
2020-07-21 20:36:08 -0700

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# MyFirstSNESGame
+To build, install WLA-DX and put folder with binaries on PATH. Then run `wla main` on command line. Will create smc file.
+To run the smc file, get an emulator (I have only tested this with snes9x so far).
---
+++ main.asm
@@ -0,0 +1,174 @@
+.INCLUDE "header.inc"
+.INCLUDE "InitSNES.asm"
+.BANK 0 SLOT 0
+.ORG 0
+.16BIT
+.SECTION "MainCode"
+SpriteData:
+  .INCBIN "main.vram"
+ColorData:
+  .INCBIN "main.pal"
+Start:
+  InitSNES
+  ; STZ $2121
+  ; LDA #%00011111
+  ; STA $2122
+  ; STZ $2122
+  ; LDA #$0F ; Enable screen in full brightness
+  ; STA $2100
+  STZ $2116
+  STZ $2117
+  LDA #$80
+  STA $2115
+  LDX #$00
+  LDA # (256 / 2 - 8)
+  STA $00
+  LDA # (224 / 2 - 8)
+  STA $01
+  STZ $02
+  STZ $03
+  JMP VRAMLoop
+VRAMLoop:
+  LDA SpriteData, X
+  STA $2118
+  INX
+  LDA SpriteData, X
+  STA $2119
+  INX
+  CPX #$80
+  BCC VRAMLoop
+  LDA #$80
+  STA $2121
+  LDX #$00
+CGRAMLoop:
+  LDA ColorData, X
+  STA $2122
+  INX
+  LDA ColorData, X
+  STA $2122
+  INX
+  CPX #$20
+  BCC CGRAMLoop
+  ; Enable 16-bit
+  CLC
+  XCE
+  ; OAM Data Setup
+  ; STZ $2102
+  ; STZ $2103
+  ; OAM Data for first sprite
+  ; LDA # (256 / 2 - 8) ; Horizontal Position
+  ; STA $2104
+  ; LDA # (224 / 2 - 8) ; Vertical Position
+  ; STA $2104
+  ; LDA #$00 ; Sprite Name
+  ; STA $2104
+  ; STZ $2104 ; Sprite Name
+  ; STZ $2104 ; No Flip, Prio 0, Palette 0
+  ; OAM Data for second sprite
+  ; STZ $2104 ; Horizontal Position
+  ; STZ $2104 ; Vertical Position
+  ; LDA #$01 ; Sprite Name
+  ; STA $2104
+  ; STZ $2104 ; No Flip, Prio 0, Palette 0
+  ; Make objects visible
+  LDA #$10
+  STA $212C
+  ; Enable screen in full brightness
+  LDA #$0F
+  STA $2100
+  ; Enable 8-bit
+  SEC
+  XCE
+  ; Enable NMI VBlank
+  ; LDA #$80
+  ; STA $4200
+  ; Enable NMI VBlank and auto-joypad read
+  LDA #$81
+  STA $4200
+GameLoop:
+  WAI
+  ; To know which bit, go to https://www.chibiakumas.com/6502/platform2.php#LessonP15 and scroll down to "The SNES Hardware" and look at JOY1H.
+  LDA #%00000001
+  CMP $4219 ; That was for this address, but the other addresses are in that website too.
+  BEQ IncreasePlayerX
+  JMP GameLoopAfterCheckedIncreasePlayerX
+GameLoopAfterCheckedIncreasePlayerX:
+  LDA #%00000010
+  CMP $4219
+  BEQ DecreasePlayerX
+  JMP GameLoopAfterCheckedDecreasePlayerX
+GameLoopAfterCheckedDecreasePlayerX:
+  LDA #%00001000
+  CMP $4219
+  BEQ DecreasePlayerY
+  JMP GameLoopAfterCheckedDecreasePlayerY
+GameLoopAfterCheckedDecreasePlayerY:
+  LDA #%00000100
+  CMP $4219
+  BEQ IncreasePlayerY
+  JMP GameLoopAfterCheckedIncreasePlayerY
+GameLoopAfterCheckedIncreasePlayerY:
+  LDA $02
+  CMP $00
+  BEQ GameLoopContinueToEnemyY
+  BCC IncreaseEnemyX
+  BCS DecreaseEnemyX
+  JMP GameLoopContinueToEnemyY
+GameLoopContinueToEnemyY:
+  LDA $03
+  CMP $01
+  BEQ GameLoopContinueAfterEnemyY
+  BCC IncreaseEnemyY
+  BCS DecreaseEnemyY
+  JMP GameLoopContinueAfterEnemyY
+GameLoopContinueAfterEnemyY:
+  JMP GameLoop
+IncreasePlayerX:
+  INC $00
+  JMP GameLoopAfterCheckedIncreasePlayerX
+DecreasePlayerX:
+  DEC $00
+  JMP GameLoopAfterCheckedDecreasePlayerX
+DecreasePlayerY:
+  DEC $01
+  JMP GameLoopAfterCheckedDecreasePlayerY
+IncreasePlayerY:
+  INC $01
+  JMP GameLoopAfterCheckedIncreasePlayerY
+IncreaseEnemyX:
+  INC $02
+  JMP GameLoop
+DecreaseEnemyX:
+  DEC $02
+  JMP GameLoop
+DecreaseEnemyY:
+  DEC $03
+  JMP GameLoop
+IncreaseEnemyY:
+  INC $03
+  JMP GameLoop
+NMIHandler:
+  LDA $4210 ; NMI Status
+  PHP
+  SEP #$20
+  STZ $2102
+  STZ $2103
+  LDA $00
+  STA $2104
+  LDA $01
+  STA $2104
+  STZ $2104
+  STZ $2104
+  STZ $2104
+  STA $2102
+  LDA $02
+  STA $2104
+  LDA $03
+  STA $2104
+  LDA #$01
+  STA $2104
+  STZ $2104
+  STZ $2104
+  PLP
+  RTI
+.ENDS
